e44d591515b3376188693f90ca378c12869ad370
tonnamb <tonnam.balankura@gmail.com>
2017-05-17 23:24:04 -0400

    Initial deployment

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :rubygems
+
+gem 'sinatra', '~> 1.4', '>= 1.4.8'
+gem "datamapper", require: "data_mapper"
+gem "slim"
+gem "sass"
+gem "dm-postgres-adapter", :group => :production
+gem "dm-sqlite-adapter", :group => :development
---
+++ main.rb
@@ -0,0 +1,61 @@
+require 'sinatra'
+require 'data_mapper'
+require 'slim'
+
+class Task
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String, :required => true
+  property :completed_at, DateTime
+  belongs_to :list
+end
+
+class List
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String, :required => true
+  has n, :tasks, :constraint => :destroy
+end
+
+DataMapper.finalize
+
+get '/' do
+  @lists = List.all(:order => [:name])
+  slim :index
+end
+
+# get '/:task' do
+#   @task = params[:task].split('-').join(' ').capitalize
+#   slim :task
+# end
+
+post '/:id' do
+  List.get(params[:id]).tasks.create params['task']
+  redirect to('/')
+end
+
+delete '/task/:id' do
+  Task.get(params[:id]).destroy
+  redirect to('/')
+end
+
+put '/task/:id' do
+  task = Task.get params[:id]
+  task.completed_at = task.completed_at.nil? ? Time.now: nil
+  task.save
+  redirect to('/')
+end
+
+post '/new/list' do
+  List.create params['list']
+  redirect to('/')
+end
+
+delete '/list/:id' do
+  List.get(params[:id]).destroy
+  redirect to('/')
+end
+
+get '/styles.css' do
+  scss :styles
+end
